b3f3917d16e542e73419f90bdd059e165f85b62a
xian797516 <51256558+xian797516@users.noreply.github.com>
2019-09-25 07:20:03 +0800

    Update main.py
    跑了一個晚上，一個都沒出現，還沒跑完

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import itertools
+
 ROLLER = [['E','K','M','F','L','G','D','Q','V','Z','N','T','O','W','Y','H','X','U','S','P','A','I','B','R','C','J'],
             ['A','J','D','K','S','I','R','U','X','B','L','H','W','T','M','C','Q','G','Z','N','P','Y','F','V','O','E'],
             ['B','D','F','H','J','L','C','P','R','T','X','V','Z','N','Y','E','I','W','G','A','K','M','U','S','Q','O'],
@@ -12,7 +14,12 @@ ROLLER_START = ['H','D','X']#起始位置
 ROLLER_ARROW = ['R','F','W','K','A']#指針位置
 PLUGBOARD = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']#接線板
 UKW_B = ['Y','R','U','H','Q','S','L','D','P','X','N','G','O','K','M','I','E','B','F','Z','C','W','V','J','A','T']#反射器
-PLAINTEXT = "IPQHUGCXZM"#明文
+#PLAINTEXT = "IPQHUGCXZM"#"IPQHUGCXZM"#明文
+PLAINTEXT = "IPQHUGCXZM"
+GUESSTEXT = "HEILHITLER"
+
+A_TO_N = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9,'K':10,'L':11,'M':12,'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,'U':20,'V':21,'W':22,'X':23,'Y':24,'Z':25}
+N_TO_A = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
 
 choose_roller = [0,0,0]
 
@@ -24,12 +31,12 @@ def reflector(list, n):
 def roller(ROLLER_LIST, start, turn, in_out, input):
     if in_out == 1:#in
         index = (start + turn) % 26
-        c = ord(ROLLER_LIST[index])-65
+        c = A_TO_N[ROLLER_LIST[index]]
         interval = 26-(start-c) if (start-c>0) else (c-start)#從起始位置到目標位置的位移量
         return interval
     elif in_out == 2:#out
         index = (start + turn) % 26
-        c = ROLLER_LIST.index(chr(index+65))#找轉盤的第幾個位置是起始位置位移後的字母
+        c = ROLLER_LIST.index(N_TO_A[index])#找轉盤的第幾個位置是起始位置位移後的字母
         interval = 26-(start-c) if (start-c>0) else (c-start)#從起始位置到目標位置的位移量
         return interval
     else:
@@ -47,8 +54,7 @@ def keyRead():
     f.read(1)
     PLUGBOARD = f.read(26)
 
-            
-def reverseList():
+'''def reverseList():
     REROLLER = [['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ],
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ],
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ],
@@ -57,15 +63,33 @@ def reverseList():
     for x in range(5):
         for index, i in enumerate(ROLLER[x]):
             REROLLER[x][DEFAULT.find(i)] = DEFAULT[index]
-        print(REROLLER[x])
+        print(REROLLER[x])'''
 
+def plugboard_gen(_p_l_t,_list):
+    p_l_t=_p_l_t.copy()
+    p_l_t.pop(p_l_t.index(_list[0]))
+    p_l_t.pop(p_l_t.index(_list[1]))
+    for i in range(len(p_l_t)):
+        if A_TO_N[_list[0]] > A_TO_N[p_l_t[0]]:
+            p_l_t.pop(0)
+        else:
+            break
+    list_n = list(itertools.combinations(p_l_t,2))
+    return p_l_t,list_n
+
+def swap(_list,_change):
+    p_l_a = _list.copy()
+    p_l_a[A_TO_N[_change[0]]], p_l_a[A_TO_N[_change[1]]] = p_l_a[A_TO_N[_change[1]]], p_l_a[A_TO_N[_change[0]]]
+    return p_l_a
 
 def main():
+    count=0
     keyRead()
+    fw=open('ans.txt','w')
     print(choose_roller, ROLLER_START, PLUGBOARD)
     ROLLER_START_TEMP = ['','','']
     roller_choose = [0,1,2,3,4]
-    for choose_roller_one in roller_choose[1:]:#3個轉盤變動且不重複
+    for choose_roller_one in roller_choose:#3個轉盤變動且不重複
         choose_roller[0] = choose_roller_one
         for choose_roller_two in roller_choose:
             if choose_roller_two == choose_roller_one:
@@ -75,39 +99,73 @@ def main():
                 if choose_roller_three == choose_roller_one or choose_roller_three == choose_roller_two:
                     continue
                 choose_roller[2] = choose_roller_three
-                for ROLLER_START_ONE in range (26):#3個轉盤起始位置變動
-                    for ROLLER_START_TWO in range (26):
-                        for ROLLER_START_THREE in range (26):
-                            CIPHERTEXT = ''
-                            ROLLER_START[0] = chr(ROLLER_START_ONE+65)
-                            ROLLER_START[1] = chr(ROLLER_START_TWO+65)
-                            ROLLER_START[2] = chr(ROLLER_START_THREE+65)
+                for roller_start_one in range (26):#3個轉盤起始位置變動
+                    for roller_start_two in range (26):
+                        for roller_start_three in range (26):
+                            ROLLER_START[0] = N_TO_A[roller_start_one]
+                            ROLLER_START[1] = N_TO_A[roller_start_two]
+                            ROLLER_START[2] = N_TO_A[roller_start_three]
                             ROLLER_START_TEMP[0]=ROLLER_START[0]
                             ROLLER_START_TEMP[1]=ROLLER_START[1]
                             ROLLER_START_TEMP[2]=ROLLER_START[2]
-                            for i in range (len(PLAINTEXT)):
-                                ROLLER_START_TEMP[2] = chr(ord(ROLLER_START_TEMP[2])+1) if(ord(ROLLER_START_TEMP[2])+1<91) else chr(65)#每輸入一個動一格
-                                if ord(ROLLER_START_TEMP[1]) == (ord(ROLLER_ARROW[1])-1 or ord(ROLLER_ARROW[1])+25):#如果中間的轉盤起始位置是指針位置前一個，最後一個轉盤跟中間轉盤都會動一格(指針為A,起始位置為Z要另外處理)
-                                    ROLLER_START_TEMP[1] = chr(ord(ROLLER_START_TEMP[1])+1) if(ord(ROLLER_START_TEMP[1])+1<91) else chr(65)
-                                    ROLLER_START_TEMP[0] = chr(ord(ROLLER_START_TEMP[0])+1) if(ord(ROLLER_START_TEMP[0])+1<91) else chr(65)
-                                if ROLLER_START_TEMP[2] == ROLLER_ARROW[2]:#
-                                    ROLLER_START_TEMP[1] = chr(ord(ROLLER_START_TEMP[1])+1) if(ord(ROLLER_START_TEMP[1])+1<91) else chr(65)
-                                in_put = PLUGBOARD[ord(PLAINTEXT[i])-65]#輸入字母經過接線板後的字母
-                                in_one = roller(ROLLER[choose_roller[2]],ord(ROLLER_START_TEMP[2])-65,ord(in_put)-65,1,PLAINTEXT[i])
-                                in_two = roller(ROLLER[choose_roller[1]],ord(ROLLER_START_TEMP[1])-65,in_one,1,PLAINTEXT[i])
-                                in_three = roller(ROLLER[choose_roller[0]],ord(ROLLER_START_TEMP[0])-65,in_two,1,PLAINTEXT[i])
-                                ref = ord(reflector(UKW_B,in_three))-65
-                                out_three = roller(ROLLER[choose_roller[0]],ord(ROLLER_START_TEMP[0])-65,ref,2,PLAINTEXT[i])
-                                out_two = roller(ROLLER[choose_roller[1]],ord(ROLLER_START_TEMP[1])-65,out_three,2,PLAINTEXT[i])
-                                out_one = roller(ROLLER[choose_roller[2]],ord(ROLLER_START_TEMP[2])-65,out_two,2,PLAINTEXT[i])
-                                out_put = PLUGBOARD.index(chr(out_one+65))
-                                if out_put is not 'HEILHITLER'[i]:
-                                   break
-                                CIPHERTEXT += chr(out_put+65)
-                            if CIPHERTEXT.find('HEILHITLER', 137)is not -1:#猜測可能有的關鍵字，找到就印出轉盤哪3個、起始位置哪3個、密文
-                                print('roller:',choose_roller)
-                                print('start:',ROLLER_START)
-                                print('result:',CIPHERTEXT)
+
+                            change_0 = list(itertools.combinations(PLUGBOARD,2))
+                            for p_change_0 in range(len(change_0)):
+                                plugboard_list_temp1,change_1 = plugboard_gen(PLUGBOARD,change_0[p_change_0])
+                                for p_change_1 in range(len(change_1)):
+                                    plugboard_list_temp2,change_2 = plugboard_gen(plugboard_list_temp1,change_1[p_change_1])
+                                    for p_change_2 in range(len(change_2)):
+                                        plugboard_list_temp3,change_3 = plugboard_gen(plugboard_list_temp2,change_2[p_change_2])
+                                        for p_change_3 in range(len(change_3)):
+                                            plugboard_list_temp4,change_4 = plugboard_gen(plugboard_list_temp3,change_3[p_change_3])
+                                            for p_change_4 in range(len(change_4)):
+                                                plugboard_list_temp5,change_5 = plugboard_gen(plugboard_list_temp4,change_4[p_change_4])
+                                                for p_change_5 in range(len(change_5)):
+                                                    plugboard_list_ans = swap(PLUGBOARD, change_0[p_change_0])
+                                                    plugboard_list_ans = swap(plugboard_list_ans, change_1[p_change_1])
+                                                    plugboard_list_ans = swap(plugboard_list_ans, change_2[p_change_2])
+                                                    plugboard_list_ans = swap(plugboard_list_ans, change_3[p_change_3])
+                                                    plugboard_list_ans = swap(plugboard_list_ans, change_4[p_change_4])
+                                                    plugboard_list_ans = swap(plugboard_list_ans, change_5[p_change_5])
+                                                    CIPHERTEXT = ''
+
+                                                    for i in range (len(PLAINTEXT)):
+                                                        ROLLER_START_TEMP[2] = N_TO_A[A_TO_N[ROLLER_START_TEMP[2]]+1] if(A_TO_N[ROLLER_START_TEMP[2]]+1<26) else N_TO_A[0]#每輸入一個動一格
+                                                        if A_TO_N[ROLLER_START_TEMP[1]] == (A_TO_N[ROLLER_ARROW[1]]-1 or A_TO_N[ROLLER_ARROW[1]]+25):#如果中間的轉盤起始位置是指針位置前一個，最後一個轉盤跟中間轉盤都會動一格(指針為A,起始位置為Z要另外處理)
+                                                            ROLLER_START_TEMP[1] = N_TO_A[A_TO_N[ROLLER_START_TEMP[1]]+1] if(A_TO_N[ROLLER_START_TEMP[1]]+1<26) else N_TO_A[0]
+                                                            ROLLER_START_TEMP[0] = N_TO_A[A_TO_N[ROLLER_START_TEMP[0]]+1] if(A_TO_N[ROLLER_START_TEMP[0]]+1<26) else N_TO_A[0]
+                                                        if ROLLER_START_TEMP[2] == ROLLER_ARROW[2]:#
+                                                            ROLLER_START_TEMP[1] = N_TO_A[A_TO_N[ROLLER_START_TEMP[1]]+1] if(A_TO_N[ROLLER_START_TEMP[1]]+1<26) else N_TO_A[0]
+                                                        in_put = plugboard_list_ans[A_TO_N[PLAINTEXT[i]]]#輸入字母經過接線板後的字母
+                                                        in_one = roller(ROLLER[choose_roller[2]],A_TO_N[ROLLER_START_TEMP[2]],A_TO_N[in_put],1,PLAINTEXT[i])
+                                                        in_two = roller(ROLLER[choose_roller[1]],A_TO_N[ROLLER_START_TEMP[1]],in_one,1,PLAINTEXT[i])
+                                                        in_three = roller(ROLLER[choose_roller[0]],A_TO_N[ROLLER_START_TEMP[0]],in_two,1,PLAINTEXT[i])
+                                                        ref = A_TO_N[reflector(UKW_B,in_three)]
+                                                        out_three = roller(ROLLER[choose_roller[0]],A_TO_N[ROLLER_START_TEMP[0]],ref,2,PLAINTEXT[i])
+                                                        out_two = roller(ROLLER[choose_roller[1]],A_TO_N[ROLLER_START_TEMP[1]],out_three,2,PLAINTEXT[i])
+                                                        out_one = roller(ROLLER[choose_roller[2]],A_TO_N[ROLLER_START_TEMP[2]],out_two,2,PLAINTEXT[i])
+                                                        out_put = plugboard_list_ans.index(N_TO_A[out_one])
+                                                        if out_put is not A_TO_N[GUESSTEXT[i]]:
+                                                           break
+                                                        CIPHERTEXT += N_TO_A[out_put]
+                                                    if CIPHERTEXT.find(GUESSTEXT)is not -1:#猜測可能有的關鍵字，找到就印出轉盤哪3個、起始位置哪3個、密文
+                                                        fw.write('roller:')
+                                                        for c_r in range(len(choose_roller)):
+                                                            fw.write(str(choose_roller[c_r]))
+                                                        fw.write('\n')
+                                                        fw.write('start:')
+                                                        for R_S in range(len(ROLLER_START)):
+                                                            fw.write(ROLLER_START[R_S])
+                                                        fw.write('\n')
+                                                        fw.write('plugboard:')
+                                                        for p_l_a_ in range(len(plugboard_list_ans)):
+                                                            fw.write(plugboard_list_ans[p_l_a_])
+                                                        fw.write('\n')
+                                                        fw.write('result:')
+                                                        fw.write(CIPHERTEXT)
+                                                        fw.write('\n')
+                                                        count+=1
+                                                        print(CIPHERTEXT,count)
 
 if __name__ == '__main__':
     main()
